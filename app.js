8389ba9d435acd0d3b218154899eca725788fab2
Fadhlaoui Raed <fadhlaouiraied@hotmail.com>
2019-11-04 07:35:41 -0800

    Types & Data
    Types & Data

--- app.js
+++ app.js
@@ -5,18 +5,37 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events=[];
+
 app.use(bodyParser.json());
 
 app.use(
   '/graphql-api',
   graphqlHttp({
     schema: buildSchema(`
+        type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            getEvents: [String!]!
+            getEvents: [Event!]!
         }
+
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(EventInput: EventInput): Event
         }
+
         schema {
             query: RootQuery
             mutation: RootMutation
@@ -24,11 +43,19 @@ app.use(
     `),
     rootValue: {
       getEvents: () => {
-        return ['Fadhlaoui', 'Testing', 'All-Night Coding'];
+        return events
       },
-      createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+      createEvent: args => {
+        const event = {
+            _id: Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: args.eventInput.date
+        }
+        console.log(args);
+        events.push(event);
+        return event
       }
     },
     graphiql: true
